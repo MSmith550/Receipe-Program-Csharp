acd3d5113e2ed8cfe7e71d693d16f9a9950d46ee
Msmith550 <krazykittie45@gmail.com>
2023-07-16 22:51:21 -0400

    Add ingredients class

--- Reciepe Book/Form1.cs
+++ Reciepe Book/Form1.cs
@@ -16,5 +16,6 @@ namespace Reciepe_Book
         {
             InitializeComponent();
         }
+
     }
 }
---
+++ Reciepe Book/Ingredients.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reciepe_Book
+{
+    class Ingredients
+    {
+        private string Name, Size;
+        private int Amount;
+
+        public Ingredients(string name, int amount, string size)
+        {
+            Name = name;
+            Size = size;
+            Amount = amount;
+        }
+
+        public string getName { get { return Name; } }
+        public int getAmount { get { return Amount; } }
+        public string getSize { get { return Size; } }
+    }
+}
